b408e8280baa9a637b183ff830c5cacb3eb76d2b
ppedvLukasK <lukask@ppedv.de>
2025-12-15 14:51:49 +0100

    M003

--- M003_MVC/Controllers/HomeController.cs
+++ M003_MVC/Controllers/HomeController.cs
@@ -21,4 +21,22 @@ public class HomeController(ILogger<HomeController> logger) : Controller
 	{
 		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 	}
+
+	public IActionResult Berechne(string z1, string z2, Rechenarten rechenart)
+	{
+		if (!float.TryParse(z1, out float zahl1) || !float.TryParse(z2, out float zahl2))
+			return BadRequest();
+
+		float ergebnis = rechenart switch
+		{
+			Rechenarten.Addieren => zahl1 + zahl2,
+			Rechenarten.Subtrahieren => zahl1 - zahl2,
+			Rechenarten.Multiplizieren => zahl1 * zahl2,
+			Rechenarten.Dividieren => zahl2 != 0 ? zahl1 / zahl2 : 0
+		};
+
+		return View("Ergebnis", ergebnis);
+	}
 }
+
+public enum Rechenarten { Addieren, Subtrahieren, Multiplizieren, Dividieren }
